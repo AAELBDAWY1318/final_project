34715a72ce9291d0c31aabf693fa081705645efb
AAELBDAWY1318 <abdullahawad1318@gmail.com>
2024-05-08 07:22:46 +0300

    i handled the error in web view and it's a just web view test

--- lib/screens/paymob_iframe/paymob_iframe.dart
+++ lib/screens/paymob_iframe/paymob_iframe.dart
@@ -1,31 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class PayMobIFrame extends StatelessWidget {
+class PayMobIFrame extends StatefulWidget {
   const PayMobIFrame({super.key});
 
   @override
+  State<PayMobIFrame> createState() => _PayMobIFrameState();
+}
+
+class _PayMobIFrameState extends State<PayMobIFrame> {
+  late final WebViewController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://flutter.dev'),
+      );
+  }
+  @override
   Widget build(BuildContext context) {
-    WebViewController controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            // Update loading bar.
-          },
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
-          onWebResourceError: (WebResourceError error) {},
-          onNavigationRequest: (NavigationRequest request) {
-            if (request.url.startsWith('https://www.youtube.com/')) {
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse('https://flutter.dev'));
     return Scaffold(
       body: WebViewWidget(controller: controller),
     );
